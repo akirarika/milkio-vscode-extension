1389301ffba09c47ea72b81ee81832d53db5ed8d
akirarika <33272184+akirarika@users.noreply.github.com>
2024-10-21 13:47:45 +0800

    🦄 improve: updated the project version number, modified the logic for starting the terminal, and added a user interaction confirmation step

--- src/bootstrap/index.ts
+++ src/bootstrap/index.ts
@@ -2,12 +2,15 @@ import * as vscode from "vscode";
 import { states } from "../states";
 
 export const bootstrap = async () => {
-  const terminal = vscode.window.createTerminal({
-    name: "milkio",
-    cwd: states.value.cwd,
-    iconPath: new vscode.ThemeIcon("debug-breakpoint-unverified"),
-    isTransient: true,
-  });
-  terminal.sendText(states.value.cookbookToml.general.start);
-  terminal.show();
+  const result = await vscode.window.showInformationMessage("This is a Milkio project, do you want to start the Cookbook?", "Yes", "No");
+  if (result === "Yes") {
+    const terminal = vscode.window.createTerminal({
+      name: "milkio",
+      cwd: states.value.cwd,
+      iconPath: new vscode.ThemeIcon("debug-breakpoint-unverified"),
+      isTransient: true,
+    });
+    terminal.sendText(states.value.cookbookToml.general.start);
+    terminal.show();
+  }
 };
